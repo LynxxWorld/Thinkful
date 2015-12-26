0104dc49eb833143a8fd17f4f9a7f2c2431d0db9
LynxxWorld <lynxx101@hotmail.com>
2015-12-26 17:13:10 +0800

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,45 @@
+import requests
+import sqlite3 as lite
+import datetime
+
+api_key = "0796f9debd90fe8ab790200ad0e9b895"
+url = 'https://api.forecast.io/forecast/' + api_key
+
+cities = { "Atlanta": '33.762909,-84.422675',
+            "Austin": '30.303936,-97.754355',
+            "Boston": '42.331960,-71.020173',
+            "Chicago": '41.837551,-87.681844',
+            "Cleveland": '41.478462,-81.679435'
+        }
+
+# Setting this equal to a variable, we fix the calculation to the point when we started the script (rather than have things move around while we're coding.)
+end_date = datetime.datetime.now() 
+
+con = lite.connect('weather.db')
+cur = con.cursor()
+
+with con:
+    cur.execute('CREATE TABLE daily_temp ( day_of_reading INT, Atlanta REAL, Austin REAL, Boston REAL, Chicago REAL, Cleveland REAL);') #use your own city names instead of city1
+
+query_date = end_date - datetime.timedelta(days=30) #the current value being processed
+
+with con:
+    while query_date < end_date:
+        cur.execute("INSERT INTO daily_temp(day_of_reading) VALUES (?)", (int(query_date.strftime('%Y-%m-%dT%H:%M:%S ')),))
+        query_date += datetime.timedelta(days=1)
+
+for k,v in cities.iteritems():
+    query_date = end_date - datetime.timedelta(days=30) #set value each time through the loop of cities
+    while query_date < end_date:
+        #query for the value
+        r = requests.get(url + v + ',' +  query_date.strftime('%Y-%m-%dT12:00:00'))
+
+        with con:
+            #insert the temperature max to the database
+            cur.execute('UPDATE daily_temp SET ' + k + ' = ' + str(r.json()['daily']['data'][0]['temperatureMax']) + ' WHERE day_of_reading = ' + query_date.strftime('%s'))
+
+        #increment query_date to the next day for next operation of loop
+        query_date += datetime.timedelta(days=1) #increment query_date to the next day
+
+
+con.close() # a good practice to close connection to database
