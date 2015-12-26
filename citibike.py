ac2eb5164554c5717054c0350092755cac0288b4
LynxxWorld <lynxx101@hotmail.com>
2015-12-26 12:32:53 +0800

    Create citibike.py

---
+++ citibike.py
@@ -0,0 +1,39 @@
+#import data from API
+import requests
+r = requests.get('http://www.citibikenyc.com/stations/json')
+
+#create list in with all station keys
+key_list = [] #unique list of keys for each station listing
+for station in r.json()['stationBeanList']:
+    for k in station.keys():
+        if k not in key_list:
+            key_list.append(k)
+
+#import list to json
+import pandas as pd
+from pandas.io.json import json_normalize
+df = json_normalize(r.json()['stationBeanList'])
+
+
+import time
+from dateutil.parser import parse
+import collections
+import sqlite3 as lite
+import requests
+
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
+for i in range(60):
+    r = requests.get('http://www.citibikenyc.com/stations/json')
+    exec_time = parse(r.json()['executionTime']).strftime("%Y-%m-%dT%H:%M:%S")
+
+    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time,))
+
+    for station in r.json()['stationBeanList']:
+        cur.execute("UPDATE available_bikes SET _%d = %d WHERE execution_time = %s" % (station['id'], station['availableBikes'], exec_time))
+    con.commit()
+
+    time.sleep(60)
+
+con.close() #close the database connection when done
